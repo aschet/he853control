cca5adaaf23a17b4110d18f150a83c7fe18bf537
Thomas Ascher <thomas.ascher@gmx.at>
2012-05-13 11:10:44 +0000

    added service app

---
+++ HE853Service/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HE853Service
+{
+    using System.ServiceProcess;
+
+    static class Program
+    {
+
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+			{ 
+				new Service() 
+			};
+
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ HE853Service/Service.cs
@@ -0,0 +1,30 @@
+﻿namespace HE853Service
+{
+    using System.Runtime.Remoting;
+    using System.Runtime.Remoting.Channels;
+    using System.Runtime.Remoting.Channels.Ipc;
+    using System.ServiceProcess;
+    
+    public partial class Service : ServiceBase
+    {
+        private HE853.Device device = new HE853.Device();
+        
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            device.Open();
+
+            ChannelServices.RegisterChannel(new IpcChannel("HE853"), false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(HE853.Device), "Device", WellKnownObjectMode.Singleton);
+        }
+
+        protected override void OnStop()
+        {
+            device.Close();
+        }
+    }
+}
