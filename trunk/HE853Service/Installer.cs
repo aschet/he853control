ea38554417d66244e6b947ca85dfaaf3b7b1720e
ashman <ashman@3b79119f-49c2-4383-b4ce-c79eb15f3c07>
2012-05-15 18:39:31 +0000

    added initial IPC implementation

---
+++ trunk/HE853Service/Installer.cs
@@ -0,0 +1,26 @@
+﻿namespace HE853Service
+{
+    using System.ComponentModel;
+    using System.ServiceProcess;
+    
+    [RunInstaller(true)]
+    public partial class Installer : System.Configuration.Install.Installer
+    {
+        public Installer()
+        {
+            InitializeComponent();
+
+            ServiceProcessInstaller process = new ServiceProcessInstaller();
+            process.Account = ServiceAccount.LocalSystem;
+
+            ServiceInstaller serviceAdmin = new ServiceInstaller();
+
+            serviceAdmin.StartType = ServiceStartMode.Manual;
+            serviceAdmin.ServiceName = "HE853";
+            serviceAdmin.DisplayName = "HE853";
+
+            Installers.Add(process);
+            Installers.Add(serviceAdmin);
+        }
+    }
+}
--- trunk/HE853Service/Service.cs
+++ trunk/HE853Service/Service.cs
@@ -1,4 +1,23 @@
-﻿namespace HE853Service
+﻿/*
+Home Easy HE853 Control Service
+Copyright (C) 2012 Thomas Ascher
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+namespace HE853Service
 {
     using System.Runtime.Remoting;
     using System.Runtime.Remoting.Channels;
@@ -16,15 +35,15 @@
 
         protected override void OnStart(string[] args)
         {
-            device.Open();
+            this.device.Open();
 
-            ChannelServices.RegisterChannel(new IpcChannel("HE853"), false);
+            ChannelServices.RegisterChannel(new IpcChannel("HE853"), true);
             RemotingConfiguration.RegisterWellKnownServiceType(typeof(HE853.Device), "Device", WellKnownObjectMode.Singleton);
         }
 
         protected override void OnStop()
         {
-            device.Close();
+            this.device.Close();
         }
     }
 }
