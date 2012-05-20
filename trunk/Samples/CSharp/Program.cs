ee4f21b8f9098326d6740b2737e8481027b89bc6
ashman <ashman@3b79119f-49c2-4383-b4ce-c79eb15f3c07>
2012-05-20 19:42:49 +0000

    CSharp sample code

--- trunk/Samples/CSharp/Program.cs
+++ trunk/Samples/CSharp/Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main(string[] args)
         {
+            HE853.Device device = new HE853.Device();
+            if (device.Open())
+            {
+                device.On(1001);
+                device.Off(1001);
+                device.Close();
+            }
         }
     }
 }
