d182ecaea1af0235724520061e7e246c4c5c617d
ashman <ashman@3b79119f-49c2-4383-b4ce-c79eb15f3c07>
2012-05-20 19:26:41 +0000

    added tlb, CPP sample code

--- trunk/Samples/CPP/CPP.cpp
+++ trunk/Samples/CPP/CPP.cpp
@@ -1,6 +1,20 @@
-#include <Windows.h>
+#import "..\..\HE853.tlb"
 
 int main(int argc, char* argv[])
 {
+	CoInitialize(NULL);
+	
+	HE853::IDevicePtr device(__uuidof(HE853::Device));
+	if (device->Open())
+	{
+	  device->On(1001);
+	  device->Off(1001);
+	  device->Close();
+	}
+
+	device->Release();
+
+	CoUninitialize();
+	
 	return 0;
 }
