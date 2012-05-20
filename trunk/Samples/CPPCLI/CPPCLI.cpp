93a935ad30d366dc4ea84fc4ffb902237a0b5db1
ashman <ashman@3b79119f-49c2-4383-b4ce-c79eb15f3c07>
2012-05-20 19:34:02 +0000

    CPPCLI sample code

--- trunk/Samples/CPPCLI/CPPCLI.cpp
+++ trunk/Samples/CPPCLI/CPPCLI.cpp
@@ -1,6 +1,14 @@
-using namespace System;
+#using "..\..\HE853.dll"
 
 int main(array<System::String ^> ^args)
 {
-    return 0;
+	HE853::IDevice^ device = gcnew HE853::Device();
+	if (device->Open())
+	{
+		device->On(1001);
+		device->Off(1001);
+		device->Close();
+	}
+
+	return 0;
 }
