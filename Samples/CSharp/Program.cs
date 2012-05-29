71b95c20898bbd1d6ee5bdc8e2dab5165413cdbe
Thomas Ascher <thomas.ascher@gmx.at>
2012-05-29 19:42:26 +0000

    updated samples

--- Samples/CSharp/Program.cs
+++ Samples/CSharp/Program.cs
@@ -1,14 +1,27 @@
 ﻿namespace CSharp
 {
+    using System;
+    using System.IO;
+
     class Program
     {
         static void Main(string[] args)
         {
             HE853.IDevice device = new HE853.Device();
-            if (device.Open())
+            try
             {
+                device.Open();
                 device.SwitchOn(1001, false);
                 device.SwitchOff(1001, false);
+                device.Close();
+            }
+            catch (FileNotFoundException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (IOException exception)
+            {
+                Console.WriteLine(exception.Message);
                 device.Close();
             }
         }
